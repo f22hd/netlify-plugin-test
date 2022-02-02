f105db1bf5d9bd881bd1e1c79c6d63e04d2bfb3d
fahd_allebdi <fa88hd@gmail.com>
2022-02-02 10:56:32 +0300

    update skipping build script

--- tools/netlify/skip-build/index.js
+++ tools/netlify/skip-build/index.js
@@ -1,30 +1,39 @@
 const { basename } = require('path');
 
 module.exports = {
-  onPreBuild: ({ utils, constants,inputs }) => {
-      console.log("onPreBuild, Site Name =>",process.env);
-      console.log("onPreBuild, inputs =>",inputs);
-      const projectName = process.env.PROJECT_NAME || basename(constants.PUBLISH_DIR);
-      const lastDeployedCommit = process.env.CACHED_COMMIT_REF
-      const latestCommit = 'HEAD'
-      const projectHasChanged = projectChanged(
-        projectName,
-        lastDeployedCommit,
-        latestCommit,
-      )
-      if (!projectHasChanged) {
-        utils.build.cancelBuild(
-          `Build was cancelled because ${projectName} was not affected by the latest changes`,
-        )
-      }
+  onPreBuild: ({ utils, constants }) => {
+    const projectName =
+      process.env.PROJECT_NAME || basename(constants.PUBLISH_DIR);
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
+    const latestCommit = 'HEAD';
+    const firstDeployment =
+      process.env.CACHED_COMMIT_REF === process.env.COMMIT_REF;
+    const projectHasChanged = projectChanged(
+      projectName,
+      lastDeployedCommit,
+      latestCommit
+    );
+
+    console.log(
+      firstDeployment,
+      process.env.CACHED_COMMIT_REF,
+      process.env.COMMIT_REF
+    );
+    if (!projectHasChanged && !firstDeployment) {
+      utils.build.cancelBuild(
+        `Build was cancelled because ${projectName} was not affected by the latest changes`
+      );
+    }
   },
-}
+};
 // eslint-disable-next-line no-unused-vars
 function projectChanged(currentProject, fromHash, toHash) {
-  const execSync = require('child_process').execSync
+  const execSync = require('child_process').execSync;
   const getAffected = `nx print-affected  --plain --base=${fromHash} --head=${toHash}`;
-  const output = execSync(getAffected).toString()
+  const output = execSync(getAffected).toString();
   //get the list of changed projects from the output
-  const changedProjects = JSON.parse(output).projects // array of affected projects
-  return changedProjects.findIndex((project) => project === currentProject) > -1
+  const changedProjects = JSON.parse(output).projects; // array of affected projects
+  return (
+    changedProjects.findIndex((project) => project === currentProject) > -1
+  );
 }
