3d8895017cde5ba5fae5bda7a26a0e11b74560a7
fahd_allebdi <fa88hd@gmail.com>
2022-06-03 19:35:40 +0300

    feat: force deployment

--- tools/netlify/skip-build/index.js
+++ tools/netlify/skip-build/index.js
@@ -1,4 +1,4 @@
-const { basename } = require('path');
+const { basename } = require('path')
 
 module.exports = {
   onPreBuild: ({ utils, constants }) => {
@@ -8,34 +8,31 @@ module.exports = {
     const latestCommit = process.env.COMMIT_REF;
     const firstDeployment =
       process.env.CACHED_COMMIT_REF === process.env.COMMIT_REF;
+    const isForcedDeployment = process.env.NX_FORCE_DEPLOYMENT || false;
+
     const projectHasChanged = projectChanged(
       projectName,
       lastDeployedCommit,
-      latestCommit
-    );
+      latestCommit,
+    )
 
-    console.log(
-      { projectName },
-      { projectHasChanged },
-      { firstDeployment },
-      { CACHED_COMMIT_REF: process.env.CACHED_COMMIT_REF },
-      { COMMIT_REF: process.env.COMMIT_REF }
-    );
-    if (!projectHasChanged && !firstDeployment) {
+    if (!projectHasChanged && !firstDeployment && !isManualDeployment(isForcedDeployment)) {
       utils.build.cancelBuild(
-        `Build was cancelled because ${projectName} was not affected by the latest changes`
-      );
+        `Build was cancelled because ${projectName} was not affected by the latest changes`,
+      )
     }
   },
-};
+}
 // eslint-disable-next-line no-unused-vars
 function projectChanged(currentProject, fromHash, toHash) {
-  const execSync = require('child_process').execSync;
-  const getAffected = `nx print-affected  --plain --base=${fromHash} --head=${toHash}`;
-  const output = execSync(getAffected).toString();
+  const execSync = require('child_process').execSync
+  const getAffected = `nx print-affected  --plain --base=${fromHash} --head=${toHash}`
+  const output = execSync(getAffected).toString()
   //get the list of changed projects from the output
-  const changedProjects = JSON.parse(output).projects; // array of affected projects
-  return (
-    changedProjects.findIndex((project) => project === currentProject) > -1
-  );
+  const changedProjects = JSON.parse(output).projects // array of affected projects
+  return changedProjects.findIndex((project) => project === currentProject) > -1
+}
+
+function isManualDeployment(isForcedDeployment){
+  return isForcedDeployment === true;
 }
